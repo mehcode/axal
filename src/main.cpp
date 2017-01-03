653171a052a0086d6abb6ce29755cf6bc331cbea
Ryan Leckey <leckey.ryan@gmail.com>
2017-01-03 00:20:26 -0800

    :shirt:

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,6 @@ int main(int argc, char** argv) {
     QApplication app(argc, argv);
     axal::Window window;
 
-    // auto menu_file = window.menuBar()->addMenu("&File");
-    // QAction action_open("&Open...", &window);
-
-    // menu_file->addAction(&action_open);
-
-    // QObject::connect(&action_open, &QAction::triggered, []() {
-    //     // TODO: Open ROM
-    // });
-
     window.show();
 
     return app.exec();
