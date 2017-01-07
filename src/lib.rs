127651ca756a47fe8d0cd4c5b500afb6e0de0aa3
Ryan Leckey <leckey.ryan@gmail.com>
2017-01-06 17:58:52 -0800

    :hammer:

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,124 @@
+extern crate libc;
 
-// Video Refresh Callback
+use std::ffi::CStr;
+use std::mem;
+
+// [Video] Refresh Fn
 //  - buffer: *const u8
 //  - width: u32
 //  - height: u32
 //  - pitch: u32
-pub type VideoRefresh = extern "C" fn(*const u8, u32, u32, u32) -> ();
+pub type VideoRefreshFn = extern "C" fn(*const u8, u32, u32, u32) -> ();
+
+// [Input] Kind
+pub enum Input {
+    Joypad,
+    Keyboard,
+}
+
+// [Input] Joypad
+pub enum Joypad {
+    A,
+    B,
+    X,
+    Y,
+
+    Select,
+    Start,
+
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+// [Input] Keyboard
+pub enum Keyboard {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z,
+
+    Num1,
+    Num2,
+    Num3,
+    Num4,
+    Num5,
+    Num6,
+    Num7,
+    Num8,
+    Num9,
+    Num0,
+
+    Backspace,
+    Tab,
+    Return,
+    Escape,
+    Space,
+
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12,
+
+    PrintScreen,
+    ScrollLock,
+    Pause,
+
+    Insert,
+    Home,
+    PageUp,
+    Delete,
+    End,
+    PageDown,
+
+    Right,
+    Left,
+    Down,
+    Up,
+}
+
+// [Input] State Fn
+//  Get the current state of input. This is updated at most once per
+//  frame.
+//
+//  - port: u8
+//  - device: u8 (Input)
+//  - id: u16 (Keyboard/Joypad)
+pub type InputStateFn = extern "C" fn(u8, u8, u16) -> bool;
 
 // Generic "Core" trait that must be implemented by a back-end
 pub trait Core {
-    // Create a new instance
-    fn new() -> Self;
-
     // Reset the environment as if it was turned off and then back on
     fn reset(&mut self);
 
@@ -23,57 +131,59 @@ pub trait Core {
     // Remove ROM (if inserted)
     fn remove_rom(&mut self);
 
-    // Store the `video_refresh` callback
-    //  - The back-end should invoke this when a "frame" of video is complete (generally during V-Blank of the guest machine)
-    fn set_video_refresh(&mut self, cb: VideoRefresh);
+    // Store the `video_refresh` Fn
+    //  - The back-end should invoke this when a "frame" of
+    //    video is complete (generally during V-Blank of the guest machine)
+    fn set_video_refresh(&mut self, cb: VideoRefreshFn);
+
+    // Store the `input_state` Fn
+    fn set_input_state(&mut self, cb: InputStateFn);
+}
+
+// Create a new (boxed) instance of a core
+unsafe fn new<T: 'static + Core + Default>() -> Box<T> {
+    Box::new(T::default())
 }
 
 // Generate extern methods to "expose" the core
 #[macro_export]
-macro_rules! ax_expose (($t:path) => {
+macro_rules! ax_core (($t:path) => {
   extern crate libc;
-  use std::mem;
-  use std::ffi::CStr;
-  use axal::Core;
-  use axal::VideoRefresh;
 
   #[no_mangle]
-  pub unsafe extern "C" fn ax_new() -> *mut libc::c_void {
-      // BUG(rust): Not sure how to use $t directly below
-      use $t as t;
-
-      mem::transmute(Box::new(t::new()))
-  }
-
-  #[no_mangle]
-  pub unsafe extern "C" fn ax_delete(ptr: *mut libc::c_void) {
-      let _drop_me: Box<$t> = mem::transmute(ptr);
-  }
-
-  #[no_mangle]
-  pub unsafe extern "C" fn ax_reset(ptr: *mut libc::c_void) {
-      (*(ptr as *mut $t)).reset();
-  }
-
-  #[no_mangle]
-  pub unsafe extern "C" fn ax_set_video_refresh(ptr: *mut libc::c_void, cb: VideoRefresh) {
-      (*(ptr as *mut $t)).set_video_refresh(cb);
-  }
-
-  #[no_mangle]
-  pub unsafe extern "C" fn ax_insert_rom(ptr: *mut libc::c_void, filename: *const libc::c_char) {
-      let filename = CStr::from_ptr(filename).to_str().unwrap();
-
-      (*(ptr as *mut $t)).insert_rom(filename);
-  }
-
-  #[no_mangle]
-  pub unsafe extern "C" fn ax_remove_rom(ptr: *mut libc::c_void) {
-      (*(ptr as *mut $t)).remove_rom();
-  }
-
-  #[no_mangle]
-  pub unsafe extern "C" fn ax_run_next(ptr: *mut libc::c_void) {
-      (*(ptr as *mut $t)).run_next();
+  pub unsafe extern "C" fn ax_new() -> *mut Core {
+      mem::transmute(new::<$t>())
   }
 });
+
+#[no_mangle]
+pub unsafe extern "C" fn ax_delete(ptr: *mut Core) {
+    let _drop_me: Box<Core> = mem::transmute(ptr);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ax_reset(ptr: *mut Core) {
+    (*ptr).reset();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ax_set_video_refresh(ptr: *mut Core, cb: VideoRefreshFn) {
+    (*ptr).set_video_refresh(cb);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ax_insert_rom(ptr: *mut Core, filename: *const libc::c_char) {
+    let filename = CStr::from_ptr(filename).to_str().unwrap();
+
+    (*ptr).insert_rom(filename);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ax_remove_rom(ptr: *mut Core) {
+    (*ptr).remove_rom();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ax_run_next(ptr: *mut Core) {
+    (*ptr).run_next();
+}
